42d63f5f28e5926943ceb44cada2f2a3b4086e18
ashok <ashokau23@gmail.com>
2024-02-01 21:43:15 +0530

    slinding.jsx - slidingCart function completed.

--- e-commerce/src/component/Navbar/SlidingCart.jsx
+++ e-commerce/src/component/Navbar/SlidingCart.jsx
@@ -1,3 +1,23 @@
 import { useContext } from "react";
 import {CartContext} from "../../context/CartProvider";
 import {ShoppingCart,X}from "phosper-react";
+import {link} from "react-router-dom";
+import "./SlidingCart.css";
+import toast from "react-hot-toast";
+
+function SlidingCart({toggleShowCart}){
+    const{cart,addProductQuantity,removeFormCart} = useContext(CartContext);
+
+    return(
+        <div className={`sliding-cart_container`}>
+        <CartTop toggleShowCart={toggleShowCart}/>
+        <CartMain 
+         cart={cart}
+         addProductQuantity ={addProductQuantity}
+         removeFormCart = {removeFormCart}
+        />
+        <CartCheckOut cart={cart} toggleShowCart={toggleShowCart} />
+        
+        </div>
+    );
+}
