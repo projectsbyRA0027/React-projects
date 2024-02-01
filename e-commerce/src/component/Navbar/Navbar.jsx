43f564bd46d39e1185eb5c2b520309c19e9f9e3a
ashok <ashokau23@gmail.com>
2024-02-01 21:32:13 +0530

    navbar.jsx bulid sucessfully.

---
+++ e-commerce/src/component/Navbar/Navbar.jsx
@@ -0,0 +1,84 @@
+import { useContext,useState } from "react";
+import {CartContext} from "../../context/CartProvider";
+import{link,Navlink} from "react-router-dom";
+import{ShoppingCart} from "phosphor-react";
+import SlidingCart from "./SlidingCart";
+import "./Navbar.css";
+
+function Navbar(){
+    const [showCart,setShowcart] = useState(false);
+
+    function toggleShowcart(){
+        setShowcart(!showCart);
+    }
+
+    return(
+        <header className={`header ${showCart ? "visible":""}`}>
+            <Navigations toggleShowcart={toggleShowcart}/>
+            <SlidingCart toggleShowCart={toggleShowCart} />
+            <CartSliderOverlay />
+        </header>
+    );
+}
+
+function CartButton({toggleShowCart}){
+    const {cart} = useContext(CartContext);
+
+    const totalCartQty = cart.reduse((totalQty,current)=>{
+        return totalQty + current.qty;
+    },0);
+
+    return(
+        <span onClick={toggleShowCart} className="cart-icon">
+            <ShoppingCart size={22}/>
+            <div className="cart-counter">{totalCartQty}</div>
+        </span>
+    );
+}
+
+function Navigations({toggleShowCart}){
+    const [isNavOpen,setIsNavOpen] = useState(false);
+
+    function handleOpenNavigation(){
+        setIsNavOpen(!isNavOpen);
+    }
+
+    return(
+        <nav className={`nav container ${isNavOpen ? "nav-open" :""}`}>
+            <span className="brand">
+              <Link to="/">Axe cloth Store</Link>
+            </span>
+            <ul className="nav-link_container">
+             <li className="nav-link">
+                <NavLink to="/">Home</NavLink>
+             </li>
+             <li className="nav-link">
+                <NavLink to="/explore/men">Men</NavLink>
+             </li>
+             <li className="nav-link">
+                <NavLink to="/explore/women">Women</NavLink>
+             </li>
+             <li className="nav-link">
+                <NavLink to="/explore/all">Explore All</NavLink>
+             </li>
+            </ul>
+            <div className="nav-secondary_btn" onClick={handleOpenNavigation}>
+                <span></span>
+                <span></span>
+                <span></span>
+               
+            </div>
+            <div className="nav-secondary">
+                <CartButton toggleShowCart={toggleShowCart} />
+            </div>
+            <div className="nav-overlay"></div>
+
+        </nav>
+    );
+}
+
+function CartSliderOverlay(){
+    return <div className="cart-slide_overlay"></div>;
+}
+
+export default Navbar;
