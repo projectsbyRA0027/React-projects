c2276d99dcfbe92a3fa00dd2f3eec838a7f38459
ashok <ashokau23@gmail.com>
2024-02-11 11:37:06 +0530

    app.js completed.

---
+++ fullstackapp1/app.js
@@ -0,0 +1,43 @@
+let express = require('express')
+ ,routes = require('./Routes')
+ ,user = require('./Routes/user')
+ ,http = require('http')
+ ,path = require('path');
+
+let session = require('express-session');
+var app = express();
+var mysql = require('mysql');
+var bodyParser = require("body-parser");
+var connection = mysql.createConnection({
+    host    : 'localhost',
+    user    : 'root',
+    password :'',
+    database  : 'reglogdetails'
+}) ;
+
+connection.connect();
+
+global.db = connection
+
+app.set('port',process.env.PORT || 8080);
+app.set('views',__dirname + '/views');
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname,'public')));
+app.use(session({
+    secret:'keyboard cat',
+    resave:false,
+    saveUninitialized:true,
+    cookie:{maxAge:60000}
+}))
+
+app.get('/',routes.index)
+app.get('/signup',user.signup);
+app.post('/signup',user.signup);
+app.get('/login',routes.index);
+app.post('/login',user.login);
+app.get('/home/dashboard',user.dashboard);
+app.get('/home/logout',user.logout);
+app.get('/home/profile',user.profile);
+app.listen(8080)
